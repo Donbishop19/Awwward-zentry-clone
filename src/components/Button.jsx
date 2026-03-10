9026cc6c16c1391ea080e1ed2a375a9ac14d6ab8
Busayo Tella <114032372+Donbishop19@users.noreply.github.com>
2026-03-10 09:07:22 -0700

    Finish the Story section by adding button and text

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass  }) => {
     <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black ${containerClass}`}>
       {leftIcon}
 
-      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
+      <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
         <div>
           {title}
         </div>
